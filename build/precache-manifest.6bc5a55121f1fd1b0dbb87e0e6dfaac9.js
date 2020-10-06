514dfe19bc11ee529de2d19f1eec54487d5e2cd6
Tai Tai <ywettai.dev@gmail.com>
2020-10-06 17:31:03 +0630

    uibuild

---
+++ build/precache-manifest.6bc5a55121f1fd1b0dbb87e0e6dfaac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e160515502faaab8318f34922ec5429f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae548037f94a99b1590",
+    "url": "/static/css/main.251313f5.chunk.css"
+  },
+  {
+    "revision": "6fa96f4e77e54634d75a",
+    "url": "/static/js/2.89f8abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89f8abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae548037f94a99b1590",
+    "url": "/static/js/main.35691f86.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
